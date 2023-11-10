e5cf248a09a10c7dba9f056e66218cb2bf51c6ba
brandnewhoho@outlook.com <brandnewhoho@naver.com>
2023-11-10 15:21:53 +0900

    feat: 회원가입 route 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Header from './components/layout/Header';
 import Home from './pages/Home';
 
@@ -9,6 +10,7 @@ const App = () => {
 			<Route element={<Header />}>
 				<Route path="/" element={<Home />} />
 				<Route path="sign-in" element={<SignIn />} />
+				<Route path="sign-up" element={<SignUp />} />
 			</Route>
 		</Routes>
 	);
