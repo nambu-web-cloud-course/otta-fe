098d13f1cdeda3f8fae7815632c864fecf3ef865
blcklamb <whenucan35@gmail.com>
2023-11-10 12:33:51 +0900

    feat: 공통 컴포넌트 생성 - common: Title(수정) - findClothingBox: Description

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,11 +1,14 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 
 export const Title = ({ text }) => {
 	return <H1>{text}</H1>;
 };
 
-const H1 = styled.h1`
-	font-weight: bold;
-	font-size: ${props => props.theme.font_sizes.lg};
-`;
+const H1 = styled('h1')(({ theme }) => {
+	return {
+		fontWeight: 'bold',
+		fontSize: theme.font_sizes.lg,
+		textAlign: 'center',
+	};
+});
