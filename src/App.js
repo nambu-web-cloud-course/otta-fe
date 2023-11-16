9947887d1c3109d5a977cdf6a4204906c00f9fcd
blcklamb <whenucan35@gmail.com>
2023-11-16 14:09:52 +0900

    fix: 의류 수거함 검색 페이지 route 하나로 통일

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SignUp from './pages/SignUp';
 import Header from './components/layout/Header';
 import Home from './pages/Home';
 import FindClothingBox from './pages/FindClothingBox';
-import Init from './components/findClothingBox/Init';
 import NoData from './components/findClothingBox/NoData';
 import CreatePost from './pages/CreatePost';
 import MyPostList from './pages/MyPostList';
@@ -21,10 +20,7 @@ const App = () => {
 				<Route path="sign-up" element={<SignUp />} />
 
 				<Route path="tip-clothing-recycle" element={<NoData />} />
-				<Route path="find-clothing-box" element={<FindClothingBox />}>
-					<Route path="" element={<Init />} />
-					<Route path="no-data" element={<NoData />} />
-				</Route>
+				<Route path="find-clothing-box" element={<FindClothingBox />} />
 				<Route path="nanum">
 					<Route path="list" element={<NanumList />} />
 					<Route path="detail" element={<NanumDetail />} />
